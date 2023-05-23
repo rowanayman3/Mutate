15cabf0cded2048aced0e5a0b074b08c49bc5a41
mahmoud7521 <mahmoudemad7521@gmail.com>
2023-05-23 21:18:03 +0300

    back

---
+++ backend/cms 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cms_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
