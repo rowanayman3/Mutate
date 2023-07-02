a23124ccf0846e848c74ea591c919a7db33f1d3a
Hassan Sayed Elkholy <116495646+EngHassanElkholy@users.noreply.github.com>
2023-07-03 00:07:29 +0300

    Update EmailDto.cs

--- backend/webapi/edits/testoken/testoken/Models/EmailDto.cs
+++ backend/webapi/edits/testoken/testoken/Models/EmailDto.cs
@@ -1,4 +1,6 @@
-﻿namespace testoken.Models
+using System;
+using MySql.Data.MySqlClient;
+namespace testoken.Models
 {
     public class EmailDto
     {
@@ -9,4 +11,30 @@
         public string Body { get; set; } = string.Empty;  
 
     }
-}
+      public class EmailRepository
+    {
+        private readonly string connectionString;
+
+        public EmailRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void SaveEmail(EmailDto email)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO emails (ToAddress, Subject, Body) VALUES (@To, @Subject, @Body)";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@To", email.To);
+                    command.Parameters.AddWithValue("@Subject", email.Subject);
+                    command.Parameters.AddWithValue("@Body", email.Body);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+      }
+ }
