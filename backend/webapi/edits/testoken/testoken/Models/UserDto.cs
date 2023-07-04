c258348788891d7cd29f7fe8eb5d5ace37a70af8
Hassan Sayed Elkholy <116495646+EngHassanElkholy@users.noreply.github.com>
2023-07-04 22:50:46 +0300

    Update UserDto.cs

--- backend/webapi/edits/testoken/testoken/Models/UserDto.cs
+++ backend/webapi/edits/testoken/testoken/Models/UserDto.cs
@@ -1,9 +1,40 @@
-﻿namespace testoken.Models
+using System.ComponentModel.DataAnnotations;
+using MySql.Data.MySqlClient;
+
+namespace testoken.Models
 {
     public class UserDto
     {
+        [Required(ErrorMessage = "Username is required.")]
+        public string? Username { get; set; }
 
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set; }
     }
-}
+
+    public class UserRepository
+    {
+        private readonly string connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void CreateUser(UserDto userDto)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO users (Username, Password) VALUES (@Username, @Password)";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", userDto.Username);
+                    command.Parameters.AddWithValue("@Password", userDto.Password);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
