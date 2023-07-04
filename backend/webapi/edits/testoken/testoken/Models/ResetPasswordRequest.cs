ab991cdd4f3ca5bc7f833b9924ba41cb35749870
Hassan Sayed Elkholy <116495646+EngHassanElkholy@users.noreply.github.com>
2023-07-04 22:49:02 +0300

    Update ResetPasswordRequest.cs

--- backend/webapi/edits/testoken/testoken/Models/ResetPasswordRequest.cs
+++ backend/webapi/edits/testoken/testoken/Models/ResetPasswordRequest.cs
@@ -1,17 +1,45 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using MySql.Data.MySqlClient;
 
 namespace testoken.Models
 {
     public class ResetPasswordRequest
     {
-
-        [Required]
+        [Required(ErrorMessage = "Token is required.")]
         public string Token { get; set; } = string.Empty;
 
-        [Required, MinLength(8)]
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         public string Password { get; set; } = string.Empty;
 
-        [Required, Compare("Password")]
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
-}
+
+    public class PasswordResetRepository
+    {
+        private readonly string connectionString;
+
+        public PasswordResetRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void ResetPassword(ResetPasswordRequest request)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE users SET Password = @Password WHERE Token = @Token";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Token", request.Token);
+                    command.Parameters.AddWithValue("@Password", request.Password);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
