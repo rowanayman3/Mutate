16aa4948f6da3ee5a66a93fcd81749b71f7429ba
Hassan Sayed Elkholy <116495646+EngHassanElkholy@users.noreply.github.com>
2023-07-04 22:52:34 +0300

    Update UserRegisterRequest.cs

--- backend/webapi/edits/testoken/testoken/Models/UserRegisterRequest.cs
+++ backend/webapi/edits/testoken/testoken/Models/UserRegisterRequest.cs
@@ -1,20 +1,53 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
+using MySql.Data.MySqlClient;
 
 namespace testoken.Models
 {
     public class UserRegisterRequest
     {
-
         public string Username { get; set; } = string.Empty;
 
-        [Required , EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; } = string.Empty;
 
-        [Required, MinLength(8)]
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         public string Password { get; set; } = string.Empty;
 
-        [Required,Compare("Password")]
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
 
+        public DateTime? BirthDate { get; set; }
     }
-}
+
+    public class UserRepository
+    {
+        private readonly string connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void RegisterUser(UserRegisterRequest user)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO users (Username, Email, Password, BirthDate) VALUES (@Username, @Email, @Password, @BirthDate)";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", user.Username);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@Password", user.Password);
+                    command.Parameters.AddWithValue("@BirthDate", user.BirthDate);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
